a072a139449cd90e238655eb6e394a16f2d238d0
Angulo <brethart69er@hotmail.com>
2021-05-14 13:01:51 -0400

    wip scrolling background

---
+++ Unity/ScoreJam13/Assets/Scripts/Behaviour/ScrollingMapGeneratorBehvaiour.cs
@@ -0,0 +1,149 @@
+namespace KasJam.ScoreJam13.Unity.Behaviours
+{
+    using UnityEngine;
+    using UnityEngine.Tilemaps;
+
+    [AddComponentMenu("AScoreJam13/ScrollingMapGenerator")]
+    public class ScrollingMapGeneratorBehvaiour : BehaviourBase
+    {
+        #region Members
+
+        public Tilemap Floor;
+
+        public TileBase[] FloorTiles;
+
+        public TileBase[] WallTiles;
+
+        protected float ScrollY { get; set; }
+
+        protected Vector2 CameraEdge { get; set; }
+
+        protected float WallWidth { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Reset()
+        {
+            Floor
+                .ClearAllTiles();
+
+            Floor.transform.position = new Vector3(0, 0, 0);
+
+            PopulateTiles();
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected void PopulateTiles()
+        {
+            var min = Floor
+                .WorldToCell(-new Vector3(CameraEdge.x + 0.64f, CameraEdge.y + 0.64f, 0));
+
+            var max = Floor
+                .WorldToCell(new Vector3(CameraEdge.x + 0.64f, CameraEdge.y + 0.64f, 0));
+
+            var size = new Vector3Int(max.x - min.x, max.y - min.y, 1);
+
+            var tileBounds = new BoundsInt(min, size);
+
+            for (int y = tileBounds.min.y; y < tileBounds.max.y; y++)
+            {
+                for (int x = tileBounds.min.x; x < tileBounds.max.x; x++)
+                {
+                    var tile = FloorTiles[0];
+                    if (x <= tileBounds.xMin + WallWidth ||
+                        x >= tileBounds.xMax - WallWidth)
+                    {
+                        tile = WallTiles[0];
+                    }
+
+                    Floor
+                        .SetTile(new Vector3Int(x, y, 0), tile);
+                }
+            }
+        }
+
+        protected void ScrollTiles()
+        {
+            Floor.transform.position += new Vector3(0, -Time.deltaTime, 0);
+
+            ScrollY += Time.deltaTime;
+
+            while (ScrollY >= 0.32f)
+            {
+                ScrollY -= 0.32f;
+
+                var bounds = Floor.cellBounds;
+
+                //Debug
+                //.Log($"CELL BOUNDS BEFORE ADDING '{Floor.cellBounds}'");
+
+                var tiles = new TileBase[bounds.size.x];
+                // add new row of cells
+                for (int x = 0; x < bounds.size.x; x++)
+                {
+                    var tile = FloorTiles[0];
+                    if (x <= WallWidth ||
+                        x >= bounds.size.x - WallWidth)
+                    {
+                        tile = WallTiles[0];
+                    }
+
+                    tiles[x] = tile;
+                }
+                var addBounds = new BoundsInt(bounds.xMin, bounds.yMax, 0, bounds.size.x, 1, 1);
+                Floor
+                    .SetTilesBlock(addBounds, tiles);
+
+                //Debug
+                //.Log($"CELL BOUNDS AFTER ADDING '{Floor.cellBounds}'");
+
+                bounds = Floor.cellBounds;
+
+                for (int i = 0; i < tiles.Length; i++)
+                {
+                    tiles[i] = null;
+                }
+
+                // delete the bottom row of cells
+                var deleteBounds = new BoundsInt(bounds.xMin, bounds.yMin, 0, bounds.size.x, 1, 1);
+                Floor
+                    .SetTilesBlock(deleteBounds, tiles);
+
+                Floor
+                    .CompressBounds();
+
+                //Debug
+                //.Log($"CELL BOUNDS AFTER DELETING '{Floor.cellBounds}'");
+            }
+        }
+
+        #endregion
+
+        #region Unity
+
+        protected void Update()
+        {           
+            ScrollTiles();
+        }
+
+        protected override void Awake()
+        {
+            Vector2 topRightCorner = new Vector2(1, 1);
+
+            CameraEdge = Camera
+                .main
+                .ViewportToWorldPoint(topRightCorner);
+
+            WallWidth = 15;
+
+            Reset();
+        }
+
+        #endregion
+    }
+}
