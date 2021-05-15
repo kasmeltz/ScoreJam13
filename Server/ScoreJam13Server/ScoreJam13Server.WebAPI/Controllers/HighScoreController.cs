b89c68201a94052d6ca1a9d7221f49e98fe3250f
Angulo <brethart69er@hotmail.com>
2021-05-14 20:04:05 -0400

    fix HighScore server?

--- Server/ScoreJam13Server/ScoreJam13Server.WebAPI/Controllers/HighScoreController.cs
+++ Server/ScoreJam13Server/ScoreJam13Server.WebAPI/Controllers/HighScoreController.cs
@@ -69,6 +69,7 @@
                     .Add(highScore);
 
                 HighScores = HighScores
+                    .OrderByDescending(o => o.Score)
                     .Take(100)
                     .ToList();
             }
