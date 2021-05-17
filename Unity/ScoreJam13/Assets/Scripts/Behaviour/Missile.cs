259dd89a9c93fed71c92fc1bd3496d6927d6afc7
Bish0pDev <ytthepienetwork@gmail.com>
2021-05-16 20:55:26 -0500

    added some changes

--- Unity/ScoreJam13/Assets/Scripts/Behaviour/Missile.cs
+++ Unity/ScoreJam13/Assets/Scripts/Behaviour/Missile.cs
@@ -10,6 +10,8 @@ namespace KasJam.ScoreJam13.Unity.Behaviours
 
         public float rotateSpeed = 200;
 
+        public float lifetime;
+
         protected Playermovement player;
 
         // Start is called before the first frame update
@@ -17,7 +19,7 @@ namespace KasJam.ScoreJam13.Unity.Behaviours
         {
             player = FindObjectOfType<Playermovement>();
             rb = GetComponent<Rigidbody2D>();
-            Invoke(nameof(Destroy), 10);
+            Invoke(nameof(Destroy), lifetime);
         }
 
         // Update is called once per frame
